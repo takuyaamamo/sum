8ac517908c51d7f5043aff17a99348d9dfca74a6
amamo vagrant <iamaama703@gmail.com>
2019-04-15 01:33:30 +0000

    n予備校sum-appと対応npmライブラリ

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+// 配列で渡された整数を全て足す関数
+const add = numbers => {
+  let result = 0;
+  for (let num of numbers) {
+    result = result + num;
+  }
+  return result;
+};
+// 配列で渡された整数を全てかける関数
+const multi = numbers => {
+    let result = 1;
+    for (let num of numbers) {
+      result = result * num;
+    }
+    return result;
+};
+
+module.exports = {
+  add : add,
+  multi : multi
+};
